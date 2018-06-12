8d3049e6be87206643d4f801258afbead2f32605
Mordnor <dorian.pitiot@gmail.com>
2018-06-12 11:44:59 +0200

    function addJeton

--- js/script.js
+++ js/script.js
@@ -3,13 +3,18 @@ var btnTable = document.createElement('table');
 btnTable.id = "button";
 var btnTr = document.createElement('tr');
 btnTable.appendChild(btnTr);
+
+// button gameButton
 for (var j = 0; j < 7; j++) {
     var btnTh = document.createElement('th');
     btnTable.lastChild.appendChild(btnTh);
+    btnTh.addEventListener("click", addJeton)
+    btnTh.innerText = j
 }
 
+ // GameBoard
 var table = document.createElement('table');
-var puissance4 = []; // game boarb
+var puissance4 = []; //
 for (var i = 0; i < 6; i++) {
 
     var tr = document.createElement('tr');
@@ -25,15 +30,19 @@ for (var i = 0; i < 6; i++) {
     }
 }
 
+
+//Add two table to Dom
 document.getElementsByTagName('body')[0].appendChild(btnTable);
 document.getElementsByTagName('body')[0].appendChild(table);
 console.log(puissance4);
 
+
+
 // game function :
-function addJeton(column){
+function positionJeton(column){
     for (var i = 0; i < 6; i++) {
         console.log(column + '>>' + i);
-        if ( puissance4[column][i] != 0){
+        if ( puissance4[i][column] != 0){
             // console.log(i-1);
             return i-1;
         }
@@ -41,6 +50,14 @@ function addJeton(column){
     // console.log(5);
     return 5;
 }
-console.log(addJeton(0));
 
 // main :
+
+
+
+function addJeton(){
+    console.log(this);
+    var column = this.innerText;
+    puissance4[positionJeton(column)][column] = "B"
+    console.log(puissance4);
+}
